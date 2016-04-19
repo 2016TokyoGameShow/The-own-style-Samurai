038561993e34a32a39237a03b65e9c02c8ec04c4
yottyansenpai <yottyan-senpai-orz@i.softbank.jp>
2016-04-19 15:53:54 +0900

    camera編集

--- src/The-own-style-Samurai/Assets/takahara/PlayerCamera.cs
+++ src/The-own-style-Samurai/Assets/takahara/PlayerCamera.cs
@@ -7,32 +7,20 @@ public class PlayerCamera : MonoBehaviour {
     private Player player;
     [SerializeField]
     private Camera mCamera;
-    [SerializeField]
-    private GameObject target;
-    private Vector3 cameraDistance;
-    private Vector3 cameraThirdParson;
-    private Vector3 rotateVelocity = new Vector3(0,3,0);
-    private float setDistance;
+    private Vector3 rotateVelocity = new Vector3(0,5,0);
+
 	// Use this for initialization
 	void Start () {
-        cameraThirdParson = new Vector3(45.0f, 0.0f, 0.0f);
-        setDistance = 5.0f;
-        target.transform.forward = player.transform.forward;
-
     }
 	// Update is called once per frame
 	void Update () {
-        target.transform.position = player.transform.position;
-        cameraDistance = target.transform.position + target.transform.forward * setDistance + new Vector3(0.0f, -setDistance, 0.0f);
+        transform.position = player.transform.position;
 
         if (Input.GetKey(KeyCode.E))
-            target.transform.eulerAngles += rotateVelocity;
+            transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, transform.eulerAngles + rotateVelocity, 5.0f * Time.deltaTime);
         if (Input.GetKey(KeyCode.Q))
-            target.transform.eulerAngles -= rotateVelocity;
+            transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, transform.eulerAngles - rotateVelocity, 5.0f * Time.deltaTime);
         if (Input.GetKeyDown(KeyCode.R))
-            target.transform.eulerAngles = player.transform.forward;
-
-        mCamera.transform.position = target.transform.position + target.transform.forward * -setDistance + new Vector3(0.0f, setDistance, 0.0f);
-        mCamera.transform.eulerAngles = target.transform.eulerAngles + cameraThirdParson;
-	}
+            transform.eulerAngles = player.transform.forward;
+    }
 }
