c6e73d524680dd61e2285af49f66bb5a9844f8ec
ralsoon <smallkirby423@yahoo.co.jp>
2016-04-19 16:08:44 +0900

    Enemy関係の追加

---
+++ src/The-own-style-Samurai/Assets/Abe/Scripts/Enemy/IEnemy.cs
@@ -0,0 +1,56 @@
+﻿// ----- ----- ----- ----- -----
+//
+// IEnemy
+//
+// 作成日：2016/04/19
+// 作成者：阿部
+//
+// <概要>
+// 敵のインターフェイスです
+//
+// ----- ----- ----- ----- -----
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[AddComponentMenu("Enemy/IEnemy")]
+[RequireComponent(typeof(Collider))]
+public abstract class IEnemy : MonoBehaviour
+{
+    [SerializeField]
+    private Animator anim;
+
+    public abstract void Attack();
+    public abstract void AttackReady();
+    public abstract void Move();
+
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public virtual void Dead()
+    {
+        //敵を吹っ飛ばしてから消去のほうがいいか
+        Destroy(this);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "EnemyBullet")
+        {
+            Dead();
+        }
+    }
+}
---
+++ src/The-own-style-Samurai/Assets/Abe/Scripts/Enemy/ShootEnemy.cs
@@ -0,0 +1,54 @@
+﻿// ----- ----- ----- ----- -----
+//
+// Archer
+//
+// 作成日：2016/04/19
+// 作成者：阿部
+//
+// <概要>
+// 遠距離攻撃をする敵です
+//
+// ----- ----- ----- ----- -----
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[AddComponentMenu("Enemy/Archer")]
+public class ShootEnemy : IEnemy
+{
+    #region 変数
+
+    [SerializeField, Tooltip("説明文")]
+    IWeapon weapon;
+
+    #endregion
+
+
+    #region プロパティ
+
+
+
+    #endregion
+
+
+    #region メソッド
+
+    public override void Attack()
+    {
+        IWeapon obj = (IWeapon)Instantiate(weapon, transform.position, Quaternion.identity);
+        
+    }
+
+    public override void AttackReady()
+    {
+        
+    }
+
+    public override void Move()
+    {
+        
+    }
+	#endregion
+}
