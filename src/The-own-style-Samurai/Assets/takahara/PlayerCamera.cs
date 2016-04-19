42051098979d9fbd2b7c3537f313e4f3fd8742bc
yottyansenpai <yottyan-senpai-orz@i.softbank.jp>
2016-04-19 14:57:09 +0900

    camera改造、プレハブで追加

--- src/The-own-style-Samurai/Assets/takahara/PlayerCamera.cs
+++ src/The-own-style-Samurai/Assets/takahara/PlayerCamera.cs
@@ -5,22 +5,34 @@ public class PlayerCamera : MonoBehaviour {
 
     [SerializeField]
     private Player player;
+    [SerializeField]
+    private Camera mCamera;
+    [SerializeField]
+    private GameObject target;
     private Vector3 cameraDistance;
-    private Vector3 cameraRotate;
+    private Vector3 cameraThirdParson;
+    private Vector3 rotateVelocity = new Vector3(0,3,0);
     private float setDistance;
 	// Use this for initialization
 	void Start () {
-        cameraRotate = new Vector3(45.0f, 0.0f, 0.0f);
+        cameraThirdParson = new Vector3(45.0f, 0.0f, 0.0f);
         setDistance = 5.0f;
-	}
-	　
+        target.transform.forward = player.transform.forward;
+
+    }
 	// Update is called once per frame
 	void Update () {
-        cameraDistance = player.transform.forward * setDistance + new Vector3(0.0f, -setDistance, 0.0f);
+        target.transform.position = player.transform.position;
+        cameraDistance = target.transform.position + target.transform.forward * setDistance + new Vector3(0.0f, -setDistance, 0.0f);
 
+        if (Input.GetKey(KeyCode.E))
+            target.transform.eulerAngles += rotateVelocity;
+        if (Input.GetKey(KeyCode.Q))
+            target.transform.eulerAngles -= rotateVelocity;
+        if (Input.GetKeyDown(KeyCode.R))
+            target.transform.eulerAngles = player.transform.forward;
 
-
-        gameObject.transform.position = player.transform.position - cameraDistance;
-        gameObject.transform.eulerAngles = player.transform.eulerAngles + cameraRotate;
+        mCamera.transform.position = target.transform.position + target.transform.forward * -setDistance + new Vector3(0.0f, setDistance, 0.0f);
+        mCamera.transform.eulerAngles = target.transform.eulerAngles + cameraThirdParson;
 	}
 }
