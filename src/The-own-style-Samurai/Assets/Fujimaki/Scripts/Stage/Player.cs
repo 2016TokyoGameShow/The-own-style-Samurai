de8b6f49d7697636510728e57d6b9e11b8d1148f
Light_Mobile <light743620@yahoo.co.jp>
2016-04-19 12:18:11 +0900

    Assetフォルダ修復

---
+++ src/The-own-style-Samurai/Assets/Fujimaki/Scripts/Stage/Player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    
+	[SerializeField,Header("移動スピード")]
+	private float speed;
+	[SerializeField]
+	private CharacterController myController;
+
+
+	private Renderer myMaterial;
+
+	void Start () {
+		myMaterial = GetComponent<Renderer> ();
+	}
+
+	void Update () {
+
+		myController.Move (new Vector3 (Input.GetAxis ("Vertical")*speed, 0,Input.GetAxis ("Horizontal")*-speed));
+
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)){
+			myMaterial.material.color = Color.red;
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			myMaterial.material.color = Color.blue;
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			myMaterial.material.color = Color.yellow;
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			myMaterial.material.color = Color.green;
+		}
+	}
+}
