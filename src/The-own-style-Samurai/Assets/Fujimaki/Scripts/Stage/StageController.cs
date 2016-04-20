e1d6559c1e7a9af855b37bed26b8b4b002c6cb2c
Light_Mobile <light743620@yahoo.co.jp>
2016-04-20 10:17:42 +0900

    プレイヤーの滑らか回転追加 必殺ゲージ追加

---
+++ src/The-own-style-Samurai/Assets/Fujimaki/Scripts/Stage/StageController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageController : MonoBehaviour {
+
+    [SerializeField]
+    private Player mPlayer;
+    [SerializeField]
+    private UIController mUiController;
+
+    public Player player { get { return mPlayer; }}
+    public UIController uiController { get { return mUiController;}}
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+	
+	}
+}
---
+++ src/The-own-style-Samurai/Assets/Fujimaki/Scripts/Stage/UIController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIController : MonoBehaviour {
+
+    [SerializeField]
+    private UITexture hpBer;
+    [SerializeField]
+    private UITexture finisherGage;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+
+    //HPの表示をセット
+    public void SetHPGage(int maxHp, int nowHp)
+    {
+        hpBer.fillAmount = (float)nowHp / (float)maxHp;
+    }
+
+    //必殺技ゲージをセット
+    public void SetFinisherGage(float value)
+    {
+        finisherGage.fillAmount = value;
+    }
+}
