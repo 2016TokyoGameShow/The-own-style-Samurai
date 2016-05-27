b126274d40d897d1524f2f0f2849bb175eb4b4f2
jiex312 <jiex312@gmail.com>
2016-05-27 15:33:15 +0900

    刀エネミー　プレイヤーを囲む

--- src/The-own-style-Samurai/Assets/Chok/Scripts/MeleeEnemy.cs
+++ src/The-own-style-Samurai/Assets/Chok/Scripts/MeleeEnemy.cs
@@ -14,28 +14,98 @@ public class MeleeEnemy : Enemy
     [SerializeField, Tooltip("アニメーションのコンポーネント")]
     Animator animator;
 
-    [SerializeField,Tooltip("行動パターン")]
-    IMeleeState meleeState;
+    [SerializeField]
+    MeleeAI m_AI;
 
+    private float speed;
+    private float angle;
     #endregion
 
-    public Animator GetAnimator
-    {
-        get { return animator; }
-    }
-
     #region メソッド
+    void Awake()
+    {
+        speed = Random.Range(2, 5);
+        angle = Random.Range(-5, 6);
+    }
     protected override void _OnMove()
     {
-        if (IsRayHitPlayer(maxDistance))
+        //if (m_AI.IsRayHitEnemy(transform.position, transform.right) ||
+        //    m_AI.IsRayHitEnemy(transform.position, transform.forward))
+        //{
+        //    for (float i = 0; i <= 1.0f; i += Time.deltaTime)
+        //    {
+        //        transform.RotateAround(player.transform.position, Vector3.up, angle);
+        //        return;
+        //    }
+        //}
+        if (m_AI.CanRayHitPlayer(player.transform.position))
         {
+            if (Vector3.Distance(transform.position, player.transform.position) < 6.0f)
+            {
+                transform.position += -transform.forward / 50;
+                return;
+            }
+            //StartStandBy();
             agent.speed = 0;
-            animator.SetFloat("Speed", agent.speed);
-            meleeState.ActionStart();
+            StartRotate();
             return;
         }
+        MoveTowardPlayer();
+    }
+
+    void StartRotate()
+    {
+        Vector3 rotateOrigin = player.transform.position;    //最初に回転中心を決める
+        StopAllCoroutines();
+        StartCoroutine(RotateAroundTarget(rotateOrigin, m_AI.GetAngle(EnemyGenerator.singleton.Angle)));
+    }
+
+    public IEnumerator RotateAroundTarget(Vector3 target, float angle)
+    {
+        float rotate = m_AI.RotateLeftOrRight(m_AI.AngleFromTarget(target), angle);
+        while (Mathf.Abs(m_AI.AngleFromTarget(target) - angle) > 10.0f)
+        {
+            transform.RotateAround(target, Vector3.up, rotate);
+            yield return null;
+        }
+        StartStandBy();
+    }
+
+    void StartStandBy()
+    {
+        StopAllCoroutines();
+        StartCoroutine(StandBy());
+    }
+    IEnumerator StandBy()
+    {
+        while (true)
+        {
+            WaitForAttack();
+            yield return null;
+        }
+    }
+
+    void WaitForAttack()
+    {
+        if (IsRayHitPlayer(6))
+        {
+            return;
+        }
+        //StartCoolTime();
+        //if (!EnemyController.singleton.Attack(gameObject, kind)) return;
+        //if (IsRayHitPlayer(maxDistance))
+        //{
+        //    agent.speed = 0;
+        //    Attack();
+        //    return;
+        //}
+        //MoveTowardPlayer();
+    }
+
+    void MoveTowardPlayer()
+    {
         agent.destination = player.transform.position;
-        agent.speed = moveSpeed;
+        agent.speed = speed;
         animator.SetFloat("Speed", agent.speed);
     }
 
@@ -64,16 +134,5 @@ public class MeleeEnemy : Enemy
         hit = CreateWeapon(weapon, attackPoint.transform.position, attackPoint.transform.rotation);
         hit.transform.parent = attackPoint.transform;
     }
-
-    public void StartAttack()
-    {
-        Attack();
-    }
-
-    public Player GetPlayer()
-    {
-        return player;
-    }
-
     #endregion
 }
