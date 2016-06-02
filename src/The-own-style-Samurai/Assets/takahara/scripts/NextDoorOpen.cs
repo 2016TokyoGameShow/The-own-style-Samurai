c513497c3ef042045ae0a18783ceec3c18d7c07b
yottyansenpai <yottyan-senpai-orz@i.softbank.jp>
2016-06-03 07:20:56 +0900

    DoorOpenNext.cs追加

---
+++ src/The-own-style-Samurai/Assets/takahara/scripts/NextDoorOpen.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextDoorOpen : MonoBehaviour {
+    [SerializeField]
+    private StageController stageController;
+    [SerializeField]
+    private int border;
+
+    private EnemyController enemycontroller;
+    // Use this for initialization
+    void Start () {
+        enemycontroller = stageController.enemycontroller;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (border <= enemycontroller.enemyDeathCount)
+        {
+            GameObject.Destroy(gameObject);
+        }
+	}
+}
