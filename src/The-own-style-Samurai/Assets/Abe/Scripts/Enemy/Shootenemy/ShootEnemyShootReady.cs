1573a2e89ce19eda7d30147b574c41b63c22d81b
ralsoon <smallkirby423@yahoo.co.jp>
2016-06-03 09:51:52 +0900

    上げ忘れ

---
+++ src/The-own-style-Samurai/Assets/Abe/Scripts/Enemy/Shootenemy/ShootEnemyShootReady.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[AddComponentMenu("MyScript/ShootEnemyShootReady")]
+public class ShootEnemyShootReady: IStateShootEnemy
+{
+
+    #region 変数
+
+    Transform enemy;
+    Transform player;
+
+    #endregion
+
+    #region プロパティ
+
+    #endregion
+
+    #region メソッド
+
+    public ShootEnemyShootReady(ShootEnemyContext context)
+    {
+        this.context = context;
+        enemy  = context.enemy.transform;
+        player = context.enemy.playerObject.transform;
+    }
+
+    public override void Move()
+    {
+        Vector3 dir = player.position - enemy.position;
+        dir.y = 0;
+        dir.Normalize();
+
+        //固定砲台と化す
+        Quaternion from = enemy.rotation;
+        Quaternion to   = Quaternion.LookRotation(dir);
+        enemy.rotation  = Quaternion.RotateTowards(from, to, context.agent.angularSpeed * Time.deltaTime);
+
+        if(Vector3.Distance(player.position, enemy.position) >= 15)
+        {
+            context.state = new ShootEnemyApproach(context);
+        }
+    }
+
+    public override void MoveEnd()
+    {
+        
+    }
+
+    #endregion
+}
