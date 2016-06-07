0093a1d4e8aaa6819a53bb16c893ea3a920c484d
Light_Mobile <light743620@yahoo.co.jp>
2016-06-07 14:01:45 +0900

    ボスリソース更新

---
+++ src/The-own-style-Samurai/Assets/Fujimaki/Scripts/Stage/WarrningSplash.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarrningSplash : MonoBehaviour {
+
+
+    private GameObject target;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = target.transform.position;
+    }
+
+    public void Initialize(float time,GameObject target)
+    {
+        this.target = target;
+        Destroy(this.gameObject, time);
+
+    }
+}
