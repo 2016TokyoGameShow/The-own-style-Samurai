5217775d42e58a2b6307feadf752f6ced7e05c05
Light_Mobile <light743620@yahoo.co.jp>
2016-06-20 18:56:39 +0900

    ソードエネミー修正版

---
+++ src/The-own-style-Samurai/Assets/Fujimaki/Scripts/Sword/StageControllerF.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StageControllerF : MonoBehaviour
+{
+    [SerializeField]
+    private EnemyGeneratorBase enemyGeneratorBase;
+
+    private int nowWave;
+    // Use this for initialization
+    void Start()
+    {
+        enemyGeneratorBase.SetWave(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddWave()
+    {
+        nowWave++;
+        enemyGeneratorBase.SetWave(nowWave);
+    }
+
+}
